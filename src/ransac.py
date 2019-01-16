41354bc32b5b756e7a985b2860f5d179594a0157
Bruno-sm <bru.1bruno@gmail.com>
2019-01-16 22:10:11 +0100

    Añade una cota superior al número de inliers aceptable y aproxima la proporción de outliers de forma adaptativa

--- src/ransac.py
+++ src/ransac.py
@@ -5,7 +5,7 @@ from matplotlib import pyplot as plt
 import functools
 
 
-# Calcula una homografía entre varias correspondencias (los puntos de pts1 van en correspondencia con los de pts2) 
+# Calcula una homografía entre varias correspondencias (los puntos de pts1 van en correspondencia con los de pts2)
 def find_homography_with_ransac(pts1, pts2):
     # Pasamos los puntos a coordenadas homogeneas
     pts1 = [[p[0], p[1], 1.0] for p in pts1]
@@ -13,24 +13,25 @@ def find_homography_with_ransac(pts1, pts2):
 
     max_inliers_prop = 0
 
-    # Resuelve la homografía con el mínimo número de puntos requeridos (m=4) escogidos al azar 
+    # Resuelve la homografía con el mínimo número de puntos requeridos (m=4) escogidos al azar
     m = 4 # Mínimo número de puntos requeridos
     H = dlt(pts1[0:m], pts2[0:m])
     H_aux = H
 
     # Calculamos N para asegurar con una probabilidad p que al menos uno de los conjuntos de muestras aleatorias no incluya un outlier
     p = 0.99 # Probabilidad que queremos de no incluir un outlier
-    v = 0.2 # Probabilidad de que un punto sea un outlier 
+    v = 0.6 # Probabilidad a priori de que un punto sea un outlier
     N = int(np.log(1-p) / np.log(1 - pow(1 - v, m)))
 
     print("Iteraciones de RANSAC: {}".format(N))
-    for i in range(0, N):
-        # Resuelve la homografía con el mínimo número de puntos requeridos (m=4) escogidos al azar 
+    i = 0
+    while i < N:
+        # Resuelve la homografía con el mínimo número de puntos requeridos (m=4) escogidos al azar
         samples_idx = np.random.choice(range(0, len(pts1)), m, False)
         samples1 = [pts1[i] for i in samples_idx]
         samples2 = [pts2[i] for i in samples_idx]
         H_aux = dlt(samples1, samples2)
-        # Determina cuantos de los puntos encajan con una tolerancia e 
+        # Determina cuantos de los puntos encajan con una tolerancia e
         e = 3
         inliers1 = []
         inliers2 = []
@@ -44,18 +45,32 @@ def find_homography_with_ransac(pts1, pts2):
             if np.linalg.norm(pts2[j] - trans_pt) < e:
                 inliers1.append(pts1[j])
                 inliers2.append(pts2[j])
-        
+
         inliers_prop = len(inliers1)/len(pts1)
         print("Proporción de inliers iteración {}: {}".format(i, inliers_prop))
-        # Si la proporción de inliers supera el máximo actual guardamos la homografía 
+
+        # Reestimamos la proporción de outliers
+        v = min(v, 1 - inliers_prop)
+        # Calculamos el número de inliers aceptable para la proporción de ouliers estimada
+        T = (1 - v) * len(pts1)
+        # Si el número de inliers supera la cota T terminamos
+        if len(inliers1) > T:
+            return dlt(np.array(inliers1), np.array(inliers2))
+
+        # Si la proporción de inliers supera el máximo actual guardamos la homografía
         if inliers_prop > max_inliers_prop:
             H = dlt(np.array(inliers1), np.array(inliers2))
             max_inliers_prop = inliers_prop
 
+        # Recalculamos N para la proporción de outliers estimada
+        N = int(np.log(1-p) / np.log(1 - pow(1 - v, m)))
+        print("Iteraciones de RANSAC recalculadas: {}".format(N))
+        i = i + 1
+
     return H
 
 
-# Resuelve la homografía para n correspondencias usando el algoritmo DLT 
+# Resuelve la homografía para n correspondencias usando el algoritmo DLT
 def dlt(pts1, pts2):
     A = build_complete_dlt_matrix(pts1, pts2)
 
@@ -63,13 +78,13 @@ def dlt(pts1, pts2):
     # Descomposición de A en valores singulares para obtener los vectores propios de A^T * A
     u, s, v = np.linalg.svd(A)
 
-    # Nos quedamos con el vector propio con menor valor singular 
+    # Nos quedamos con el vector propio con menor valor singular
     h = v[-1::][0]
     # Normalizamos el vector
     h = h * (1/np.linalg.norm(h))
     # Lo ponemos en forma de matriz 3x3
     h = np.reshape(h, (3, 3))
-    return h 
+    return h
 
 
 # Construye la matriz de DLT para un conjunto de correspondencias
