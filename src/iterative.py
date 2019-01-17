7b44b4dd6b0eadfb194a3b9564d59485e4daba53
InakiMadi <inakimadi@gmail.com>
2019-01-17 22:29:33 +0100

    más comentarios de iterative

--- src/iterative.py
+++ src/iterative.py
@@ -15,16 +15,17 @@ P_0:
     Inicialización.
     
 f:
-    Función coste.
+    f(P) = (H x_1, ..., H x_n) = (f_1(x_1,H), ... , f_n(x_n,H))
 
 X:
     X = f(P), con X el vector medido que aproxima el valor de verdad.
 
 e:
+    Función coste.
     e_0 = f(P_0) - X
 
 J:
-    J es la Jacobiana (df/dP).
+    J es la Jacobiana (df/dP). Matriz nx9.
     
 Asumimos que f es aproximada a P_0 por f(P_0 + Delta) = f(P_0) + J Delta.
 
@@ -55,6 +56,8 @@ Donde g_{pp} es la matriz Hessiana de g, y g_p es el gradiente de g.
 con e(P) = f(P) - X.
 También, se puede computar como
 
+e_p = J.
+
 NEWTON:
     g_{pp} = e_p^T e_p + e_{pp}^T e
     g_p = e_p^T e.
@@ -72,5 +75,46 @@ GRADIENT DESCENT:
     
 
 
-*** ***
+*** LM ***
+
+TEORÍA Delta:
+    Se obtiene de resolver ahora: (J^T J + Lambda I) Delta = - J^T e, para algún valor de Lambda que varía de iteración a iteración.
+Normalmente, el valor inicial de Lambda es 10^(-3)*(Media de la diagonal de J^T J).
+
+Si el valor de Delta hallado reduce el error, entonces el incremento es aceptado y lambda = lambda/10.
+Si el valor de Delta hallado aumenta el error, entonces lambda = lambda*10 y se repite.
+
+Cuando se acepta un incremento, entonces LM da 1 iteración.
+
+RAZONAMIENTO -> pág 601
+
+
+
+
+
+
+
+
+-----------------------------
+
+CHECA PABLO:
+
+P:
+    Vector de parámetros de R^N a R^M.
+    
+P_0:
+    Inicialización.
+    
+f:
+    f(P) = función de Sampson
+
+X:
+    X = valor al que se minimiza. X=0.
+
+e:
+    f(P) - X = f(P)
+
+J:
+    J es la Jacobiana (df/dP). Matriz 1x9
+
 """
