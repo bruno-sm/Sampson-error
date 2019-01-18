d01dd9f6953700a7c762516cb492728d3e4e0981
bruno-sm <bru.1bruno@gmail.com>
2019-01-18 02:16:59 +0100

    Parece que newton funciona pero no estoy seguro

--- src/newton.py
+++ src/newton.py
@@ -7,19 +7,16 @@ from derivative import partial_derivative
 def find_homography_with_newton(pts1, pts2):
     H = find_homography_with_ransac(pts1, pts2)
     f = lambda P : sampson_error(pts1, pts2, np.reshape(P, (3, 3)))
-    vectorized_f = np.vectorize(
-        lambda h1, h2, h3, h4, h5, h6, h7, h8, h9 : f(np.matrix([h1, h2, h3, h4, h5, h6, h7, h8, h9]).T)
-    )
-    P = np.reshape(H, (1, 9))
+    P = np.reshape(H, (9, 1))
     ε = np.matrix([f(P)])
+    print("Error: {}".format(ε))
 
     Δ = np.zeros((1,1)) + np.inf
     while np.mean(Δ) > 0:
-        J = np.matrix([partial_derivative(f, np.asarray(P)[0], i) for i in range(len(P))]) 
+        J = np.matrix([partial_derivative(lambda v: f(np.matrix(v)), np.asarray(P.T)[0], i) for i in range(len(P))]) 
         Δ = -np.linalg.pinv(J) * ε
-        print("Δ: {}".format(Δ))
         P = P + Δ
         ε = np.matrix([f(P)]) 
-        print("Error: {}".format(np.sum(ε)))
+        print("Error: {}".format(ε))
 
     return np.reshape(P, (3, 3))
